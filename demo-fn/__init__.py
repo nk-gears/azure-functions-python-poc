9c5bade6cb0f7470033441006db56c0616dc0e31
Shakir Shakeel <shakir.s@softcrylic.co.in>
2019-07-31 15:48:23 +0530

    Adding azure blob conn

--- demo-fn/__init__.py
+++ demo-fn/__init__.py
@@ -1,9 +1,15 @@
 import logging
 
 import azure.functions as func
-import pandas as pd
-ls=["name","age"]
-df=pd.DataFrame(ls)
+#import pandas as pd
+#ls=["name","age"]
+#df=pd.DataFrame(ls)
+from azure.storage.blob import BlockBlobService
+accountey="MkCyzS20s5n8LP5ukutsmNjdnenV6iVaAAJEDXc9xb3xWidAcfCpvhFiovAcdYCOReLkQjSzxb7w7D3W5wXq2Q=="
+accountName="scifunctionapp"
+containerName="democontainer"
+df =  pd.read_csv(pd.compat.StringIO(((BlockBlobService(account_name=accountName,account_key=accountey)).get_blob_to_text(containerName,"creditcard.csv")).content),encoding='utf-8',error_bad_lines=False)
+x=df.head()
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
@@ -20,7 +26,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello {name} {df[0]}!")
+        return func.HttpResponse(f"Hello {name} {x}!")
     else:
         return func.HttpResponse(
              "Please pass a name on the query string or in the request body",
