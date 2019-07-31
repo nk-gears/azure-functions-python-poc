7ee4b9081fa20151264bc0e942085bb7ebc4704b
Shakir <shakirshakeelzargar11@gmail.com>
2019-07-31 22:05:21 +0530

    adding modules

--- demo-fn/__init__.py
+++ demo-fn/__init__.py
@@ -42,7 +42,37 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         )
         
 '''
-from modules import StopWatch
 
-with StopWatch() as sw:
-    print("[*] Elapsed: {0:.2f}s".format(sw.elapsed_s))
+
+
+from modules import StopWatch
+import logging
+
+import azure.functions as func
+import pandas as pd
+ls=["name","age"]
+df=pd.DataFrame(ls)
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        with StopWatch() as sw:
+            t=("[*] Elapsed: {0:.2f}s".format(sw.elapsed_s))
+            return func.HttpResponse(f"Hello {name} {t}")
+    else:
+        return func.HttpResponse(
+             "Please pass a name on the query string or in the request body",
+             status_code=400
+        )
+
