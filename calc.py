79773d3d773669e1c26b1bcbc572e3ff0b3a190d
Shakir Shakeel <shakir.s@softcrylic.co.in>
2019-07-31 19:31:10 +0530

    Adding time

---
+++ calc.py
@@ -0,0 +1,2 @@
+import test
+test.add(1, 1)
--- demo-fn/__init__.py
+++ demo-fn/__init__.py
@@ -1,5 +1,5 @@
 import logging
-
+from stopwatch import StopWatch
 import azure.functions as func
 import pandas as pd
 #ls=["name","age"]
@@ -23,13 +23,15 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        try:
-            df =  pd.read_csv(pd.compat.StringIO(((BlockBlobService(account_name=accountName,account_key=accountey)).get_blob_to_text(containerName,"demodata.csv")).content),encoding='utf-8',error_bad_lines=False)
-            x=df.head()
-        except Exception as e:
-            return func.HttpResponse(f"Hello  The error is {e}!")
-        else:
-            return func.HttpResponse(f"Hello {x}!")
+        with StopWatch() as sw:
+            try:
+                df =  pd.read_csv(pd.compat.StringIO(((BlockBlobService(account_name=accountName,account_key=accountey)).get_blob_to_text(containerName,"demodata.csv")).content),encoding='utf-8',error_bad_lines=False)
+                x=df.head()
+                tiime=("[*] Elapsed: {0:.2f}s ".format(sw.elapsed_s))
+            except Exception as e:
+                return func.HttpResponse(f"Hello  The error is {e}!")
+            else:
+                return func.HttpResponse(f"Hello {x} THE TIME TAKEN IS {tiime}!")
 
 
     else:
